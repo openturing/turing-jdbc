f5c1cbcce3542052aab8387b06bc2a89fc870a4f
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2017-10-28 11:08:09 -0200

    - Initial Commit

---
+++ src/main/java/com/viglet/turing/tool/jdbc/JDBCImportTool.java
@@ -0,0 +1,9 @@
+package com.viglet.turing.tool.jdbc;
+
+public class JDBCImportTool {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Viglet Turing JDBC Import Tool.");		
+	}
+}
---
+++ turing-jdbc.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+./gradlew -PmainClass=com.viglet.turing.tool.jdbc.JDBCImportTool execute
